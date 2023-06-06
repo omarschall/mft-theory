03a15b413eae70d4ae927e7e9c1ba188db165e5c
omarschall <owen.marschall@gmail.com>
2023-06-06 14:02:38 -0400

    Lots of stuff not sure

--- empirics/dimensionality.py
+++ empirics/dimensionality.py
@@ -14,3 +14,36 @@ def compute_participation_coefficient(cov):
     n_features = cov.shape[0]
     frob_norm = np.sum(np.square(cov))
     return np.trace(cov)**2 / frob_norm / n_features
+
+def random_derangement(n):
+    while True:
+        v = [i for i in range(n)]
+        for j in range(n - 1, -1, -1):
+            p = np.random.randint(j+1)
+            if v[p] == j:
+                break
+            else:
+                v[j], v[p] = v[p], v[j]
+        else:
+            if v[0] != 0:
+                return tuple(v)
+
+def compute_approximate_participation_coefficient(X, demean=True,
+                                                  n_derangments=10):
+    """From a data matrix X, compute the PR by estimating off-diagonals of
+    X. X must have shape (samples, features)."""
+
+    if demean:
+        X = X - X.mean(0)
+
+    pr = []
+    for i in range(n_derangments):
+        avg_X_var_squared = np.square((X * X).sum() / (X.shape[0] - 1))
+        avg_X_squared_var = np.square((X * X).sum(0) / (X.shape[0] - 1)).sum()
+
+        shuffle_idx = random_derangement(X.shape[1])
+        psi_00_estimate = np.square((X * X[:, shuffle_idx]).sum(0) / (X.shape[0] - 1)).sum()
+
+        pr.append(avg_X_var_squared / (avg_X_squared_var + X.shape[1] * psi_00_estimate) / X.shape[1])
+
+    return np.mean(pr)
--- theory/cavity_method.py
+++ theory/cavity_method.py
@@ -1,12 +1,16 @@
 import numpy as np
+from .basic_dmft import *
+from .theory_utils import gaussian_integral
+from functools import partial
 
-def psi_phi_in_fourier_space(omega_1, omega_2, alpha, nu, Delta_omega):
+def psi_phi_in_fourier_space(omega_1, omega_2, alpha, g, Delta_omega):
     """Calculate the cross-covariance in Fourier space for the activations, ie
     Eq. 6 in David's paper with a = phi.
 
     The discretization used in the fft of Delta_omega must match that of any
     vectorized inputs to the arguments omega_1 and omega_2."""
 
+    nu = alpha**2 * g**2
     gamma_phi = alpha**4
     Phi_phi = np.multiply.outer((1 + omega_1**2), (1 + omega_2**2)) / (nu**2)
 
@@ -16,3 +20,21 @@ def psi_phi_in_fourier_space(omega_1, omega_2, alpha, nu, Delta_omega):
 
     return F1 * F2 * F3
 
+def compute_Delta_omega(g, T=10, dT=0.01):
+
+    Delta_0 = solve_for_Delta_0(g=g, Delta_0_init=70)
+    time, Delta_T, _ = solve_for_Delta_T(g=g, Delta_0=Delta_0, T=T, dT=dT)
+
+    Delta_omega = np.fft.fft(Delta_T)
+    omega = np.fft.fftfreq(n=int(T/dT),d=dT)
+    return omega, Delta_omega
+
+def phi_prime_x(z, Delta_0):
+
+    return 1/np.cosh(np.sqrt(Delta_0)*z)**2 * np.sqrt(Delta_0)
+
+def compute_alpha(g):
+
+    Delta_0 = solve_for_Delta_0(g=g, Delta_0_init=70)
+    f = partial(phi_prime_x, Delta_0=Delta_0)
+    return gaussian_integral(f)
